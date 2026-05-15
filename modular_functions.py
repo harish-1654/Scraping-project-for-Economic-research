6687da2c75003570964f35f838cf42e89f548392
Harish <harish.arolla@bizacuity.com>
2026-05-15 15:14:07 +0530

    added proper tasks and instructions in readme

--- modular_functions.py
+++ modular_functions.py
@@ -1,3 +1,4 @@
 #all the functions required for the project are recorded here
 #they will be imported in the main file.
 
+
--- scraper.py
+++ scraper.py
@@ -2,3 +2,14 @@
 import modular_functions as functions
 
 
+"""
+I'll be creating global variables here, and passing them to the functions as params. 
+Since its a small project, It is scalable and modular.
+core should be error handling.
+"""
+
+def extract_data(url):
+    """
+    We'll be using this function to extract tabular data from the given url. since it is tabular data in htlm, we'll directly parse it from pandas html function.
+    """
+    
